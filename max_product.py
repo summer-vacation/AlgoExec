703e6129c9d3a338b7b2de4456de46f9ad178302
jynnezhang <jynnezhang@tencent.com>
2019-12-07 18:50:24 +0800

    max_product 1207 todo: 超时

---
+++ max_product.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+
+   File Name：     max_product
+   Author :       jing
+   Date：          2019-12-07
+   乘积最大子序列
+   https://leetcode-cn.com/explore/interview/card/top-interview-quesitons-in-2018/264/array/1126/
+"""
+
+
+class Solution:
+    """
+    maxProduct1() 超时！！！
+    """
+    def maxProduct1(self, nums) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        else:
+            max = 0
+            pre = 0
+            for i in range(len(nums)):
+                if nums[i] == 0:
+                    continue
+                pre = nums[i]
+                if max < pre:
+                    max = pre
+                for j in range(i+1, len(nums)):
+                    if nums[j] == 0:
+                        break
+                    pre = pre * nums[j]
+                    if max < pre:
+                        max = pre
+
+            return max
+
+
+if __name__ == '__main__':
+    print(Solution().maxProduct1([-2, 0, -1]))
+
