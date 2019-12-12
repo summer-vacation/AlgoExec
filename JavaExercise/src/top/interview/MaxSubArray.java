ea4bdf71698eb53b6ca5142173671361e1871d2a
jynnezhang <jynnezhang@tencent.com>
2019-12-12 21:20:27 +0800

    Max SubArray -java

---
+++ JavaExercise/src/top/interview/MaxSubArray.java
@@ -0,0 +1,51 @@
+package top.interview;
+
+
+public class MaxSubArray {
+    public MaxSubArray(){
+
+    }
+
+    /**
+     * 前面的最大和+当前数  小于 当前数，max = 当前数
+     * @param nums
+     * @return
+     */
+    public int maxSubArray_dp(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }else if (nums.length == 1){
+            return nums[0];
+        }
+        int length = nums.length;
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < length; i++){
+            dp[i] = Math.max(nums[i], dp[i-1] + nums[i]);
+            if(max < dp[i]){
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+
+    public int maxSubArray(int[] nums) {
+        if(nums==null || nums.length==0) return 0;
+        int sumSoFar = 0;
+        int max = Integer.MIN_VALUE;
+        for(int num:nums){
+            sumSoFar+=num;
+            if(num>sumSoFar){
+                sumSoFar = num;
+            }
+            max = Math.max(max,sumSoFar);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{-2,-1};
+        System.out.println(new MaxSubArray().maxSubArray_dp(input));
+    }
+}
