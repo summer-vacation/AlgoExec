613bd3653346224a1519736df1d8a36aaaee68be
jynnezhang <jynnezhang@tencent.com>
2019-12-16 22:08:34 +0800

    Valid Anagram -java

---
+++ JavaExercise/src/top/interview/ValidAnagram.java
@@ -0,0 +1,28 @@
+package top.interview;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() ==0 && t.length() == 0){
+            return true;
+        }else if(s.length() != t.length()) {
+            return false;
+        } else {
+            StringBuilder sb = new StringBuilder(s);
+            for (int i = 0; i < t.length(); i++) {
+                int index = s.indexOf(t.charAt(i));
+                if (index == -1) {
+                    return false;
+                } else{
+                    s = sb.deleteCharAt(index).toString();
+                    // System.out.println(s);
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "ccac", t = "aacc";
+        System.out.println(new ValidAnagram().isAnagram(s, t));
+    }
+}
