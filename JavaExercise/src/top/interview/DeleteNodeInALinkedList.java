1c394eb3ac04c24765b69cd53f18d06c80b4c827
jynnezhang <jynnezhang@tencent.com>
2019-12-18 14:39:52 +0800

    Delete Node In A LinkedList -java

---
+++ JavaExercise/src/top/interview/DeleteNodeInALinkedList.java
@@ -0,0 +1,33 @@
+package top.interview;
+
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        if (node.next != null){
+            if (node.next.next != null){
+                node.val = node.next.val;
+                node.next = node.next.next;
+            }else {
+                node.val = node.next.val;
+                node.next = null;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode root = new ListNode(4);
+        root.next = new ListNode(5);
+        root.next.next = new ListNode((1));
+        root.next.next.next = new ListNode(9);
+        new DeleteNodeInALinkedList().deleteNode(root);
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
