243bfe1b325893c5da63f78f4c0ee5033c983993
JingChangzz <280806137@qq.com>
2020-01-03 12:09:22 +0800

    First Bad Version -java

---
+++ JavaExercise/src/top/interview/SortingAndSearching/FirstBadVersion.java
@@ -0,0 +1,24 @@
+package top.interview.SortingAndSearching;
+
+/**
+ * https://leetcode.com/explore/featured/card/top-interview-questions-easy/96/sorting-and-searching/774/
+ * 找到第一个isBadVersion() true的版本
+ * 二分查找，，如果isBadVersion(mid)==true, 就找low-mid
+ * 如果isBadVersion(mid)==false, 就找mid-high
+ */
+public class FirstBadVersion {
+
+    public int firstBadVersion(int n) {
+        return helper(1, n);
+    }
+
+    public int helper(int low, int high) {
+        if (low >= high) return low;
+        else {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid)) return helper(low, mid);
+            else return helper(mid+1, high);
+        }
+    }
+
+}
