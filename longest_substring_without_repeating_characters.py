b5c13ffe869db0d3bf5f9f1ce956a145cd01eb46
JingChangzz <280806137@qq.com>
2020-03-16 19:32:36 +0800

    longest_substring_without_repeating_characters python

---
+++ longest_substring_without_repeating_characters.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+
+   File Name：     longest_substring_without_repeating_characters
+   Author :       jing
+   Date：          2020/3/16
+
+    https://leetcode.com/problems/longest-substring-without-repeating-characters/
+"""
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_len = 0
+        temp = []
+        lens = 0
+        for char in s:
+            if char not in temp:
+                lens += 1
+                temp.append(char)
+            else:
+                temp.append(char)
+                temp = temp[temp.index(char)+1:]
+                lens = len(temp)
+            max_len = max(max_len, lens)
+        return max_len
+
+
+if __name__ == '__main__':
+    print(Solution().lengthOfLongestSubstring("bcbdf"))
+
